d46040f39aac1c724e3d0504156fd31333965d73
lcyljy <35355087+lcyljy@users.noreply.github.com>
2021-10-14 11:08:10 +0900

    test

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './Home.scss'
+
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+                <main>
+                    <div className="container check">
+                        <a href="https://lcyljy.github.io/react/portfolio3"><div className="mainContainer check"></div></a>
+                    </div>
+                </main>
+            </div>
+        )
+    }
+}
+
+export default Home
